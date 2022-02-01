3e96f8a18cc9208d4ecb27b7c1f2cf6ff81f4d80
A7-4real <a7.4real@gmail.com>
2022-02-02 02:48:15 +0530

    added homepage html, css and js file

---
+++ homepage/script.js
@@ -0,0 +1,17 @@
+// js
+
+// document object represents the web page
+// if you want want to access any element in an HTML page, you always start with accessing the document object.
+
+// The addEventListener() method attaches event handler to the specified element.
+// element.addEventListener(event, function, useCapture)
+
+// The DOMContentLoaded event fires when the initial HTML document has
+// been completely laoded and parsed, without waiting for
+// styesheet, images and subframes to finish laoding.
+document.addEventListener("DOMContentLoaded", init);
+
+async function init() {
+  let response = await fetch("./data/countries.json");
+  let countries = await response.json();
+}
