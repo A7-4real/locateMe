8ee649be871fde118589c22fe692f3b3d16e6ac3
A7-4real <97912085+A7-4real@users.noreply.github.com>
2022-03-04 22:42:19 +0530

    Update script.js

--- homepage/script.js
+++ homepage/script.js
@@ -12,5 +12,45 @@
 document.addEventListener("DOMContentLoaded", init);
 
 async function init() {
-    let response = await fetch("./data/flag-name.json");
-    let countries = await response.json();
+  let response = await fetch("./data/flag-name.json");
+  let countries = await response.json();
+
+  let response = await fetch("./data/streetview-countries.json");
+  let streetviewCounntries = await response.json();
+
+  console.log(streetviewCounntries);
+
+  let html = "";
+  for (let country in countries) {
+    let flagCode = flagNames[country];
+    if (!flagCode || !streetviewCounntries.includes(country)) continue;
+
+    html += `
+            <div class="map">
+                <div class="map-background" style="background-image: url(data/thumbnails/flags/${flagCode.toLowerCase()}.svg)"></div>
+                <div class="map-text>
+                    <div class="map-title">${country}</div>
+                    <div class="map-actions">
+                        <a class="map-play" href="./play#${encodeURI(
+                          country
+                        )}>Play</a>
+                        <a class="map-score" href="./highscore#${encodeURI(
+                          country
+                        )}>Scores</a>
+                    </div>
+                </div>
+            </div>
+        `;
+  }
+  document.querySelector(".country-maps").innerHTML = html;
+}
+
+function playMyArea(e) {
+  navigator.geolocation.getCurrentPosition((position) => {
+    let radius = document.querySelector(".radius-input").value;
+    location.href = `./play/#area#${position.coords.latitude}#${radius}`;
+  });
+}
+
+
+
